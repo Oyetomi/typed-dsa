06681e768e908e897592c6a4ebc9e7589ad94774
Oyetomi <oyetomiabiodun@yahoo.com>
2025-01-07 02:02:51 +0100

    206-Reverse Linked List

---
+++ 206-Reverse Linked List/main.ts
@@ -0,0 +1,23 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let current = head;
+  let newHead = null;
+  let nextNode = null;
+
+  while (current !== null) {
+    nextNode = current.next;
+    current.next = newHead;
+    newHead = current;
+    current = nextNode;
+  }
+
+  return newHead;
+}
